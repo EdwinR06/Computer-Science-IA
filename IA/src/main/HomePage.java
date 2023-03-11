aeff6b1fbf197c098362efb30bbb15a7b9334786
Edwin Ryerson <eryerson06@gmail.com>
2023-03-10 18:40:57 -0700

    Add save functionality to HomePage exit()

--- IA/src/main/HomePage.java
+++ IA/src/main/HomePage.java
@@ -1,7 +1,10 @@
+package main;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class HomePage extends JFrame implements ActionListener {
 
@@ -9,10 +12,16 @@ public class HomePage extends JFrame implements ActionListener {
     private JButton teacherLoginButton;
     private JButton studentLoginButton;
     private Library library;
-
-    public HomePage(Library library) {
+    private String bookDir;
+    private String teacherDir;
+    private String usersDir;
+    public HomePage(Library library, String bookDir, String teacherDir, String usersDir) {
         super("Classroom Library System");
         this.library = library;
+        this.bookDir = bookDir;
+        this.teacherDir = teacherDir;
+        this.usersDir = usersDir;
+
         // Create buttons
         exitButton = new JButton("Exit");
         teacherLoginButton = new JButton("Teacher");
@@ -40,7 +49,22 @@ public class HomePage extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == exitButton) {
             // Exit the program
-            System.exit(0);
+            try {
+                library.saveBooks(bookDir);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            try {
+                library.saveTeacher(teacherDir);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            try {
+                library.saveUsers(usersDir);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            dispose();
         } else if (e.getSource() == teacherLoginButton) {
             // Go to teacher login window
             new TeacherLoginPage(library);
