1bb64977f93459fe186db12ce4675712be84bece
Edwin Ryerson <eryerson06@gmail.com>
2023-03-23 10:43:50 -0600

    Add labels to teacher login page

--- IA/src/main/TeacherLoginPage.java
+++ IA/src/main/TeacherLoginPage.java
@@ -8,7 +8,9 @@ import java.awt.event.ActionListener;
 public class TeacherLoginPage extends JFrame implements ActionListener {
 
     private Library library;
+    private JLabel usernameLabel;
     private JTextField usernameInputField;
+    private JLabel passwordLabel;
     private JTextField passwordInputField;
     private JButton submitButton;
 
@@ -19,7 +21,9 @@ public class TeacherLoginPage extends JFrame implements ActionListener {
         this.library = library;
 
         // Create input field and submit button
+        usernameLabel = new JLabel("Username:");
         usernameInputField = new JTextField(20);
+        passwordLabel = new JLabel("password");
         passwordInputField = new JTextField(20);
         submitButton = new JButton("Login");
 
@@ -27,7 +31,9 @@ public class TeacherLoginPage extends JFrame implements ActionListener {
 
         // Add input field and submit button to content pane
         JPanel contentPane = new JPanel(new FlowLayout());
+        contentPane.add(usernameLabel);
         contentPane.add(usernameInputField);
+        contentPane.add(passwordLabel);
         contentPane.add(passwordInputField);
         contentPane.add(submitButton);
         setContentPane(contentPane);
@@ -44,7 +50,7 @@ public class TeacherLoginPage extends JFrame implements ActionListener {
             String password = passwordInputField.getText();
             boolean loggedIn = library.loginTeacher(username, password);
             if(loggedIn) {
-                //new TeacherHomePage(library);
+                new TeacherHomePage(library);
                 dispose(); // Close this window
             } else {
                 JOptionPane.showMessageDialog(this, "Please enter valid username and password!"); // Display a message dialog with the username
