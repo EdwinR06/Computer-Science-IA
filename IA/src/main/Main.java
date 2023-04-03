2e34d5650cc2c930f569a68d5d3c91581c51fa1a
Edwin Ryerson <eryerson06@gmail.com>
2023-04-02 22:00:51 -0600

    Clean up Main

--- IA/src/main/Main.java
+++ IA/src/main/Main.java
@@ -3,23 +3,10 @@ package main;
 import java.io.IOException;
 
 public class Main {
+    private final static String root = "src/main/";
     public static void main(String[] args) throws IOException {
-        String[] dirs = {"/Users/edwinryerson/Documents/GitHub/Computer-Science-IA/IA/src/main/books.csv", "/Users/edwinryerson/Documents/GitHub/Computer-Science-IA/IA/src/main/teacher.csv", "/Users/edwinryerson/Documents/GitHub/Computer-Science-IA/IA/src/main/users.csv"};
+        String[] dirs = {root + "books.csv", root + "teacher.csv", root + "users.csv"};
         Library library = new Library(dirs);
-
-
-        //library.addUser(new Student("student3"));
-
-        //library.loginTeacher("admin", "password");
-        System.out.println(library.getCurrentUser().getUsername());
-        //library.loginStudent("student");
-        //System.out.println(library.getCurrentUser().getUsername());
-        
-        //library.addBook(new Book("hhg", "hhg", "hjhjf", 40));
-        //library.printBooks();
-        //library.saveBooks("/Users/edwinryerson/IdeaProjects/IA/src/books.csv");
-
-        //library.saveUsers("/Users/edwinryerson/IdeaProjects/IA/src/users.csv");
 
         new HomePage(library);
     }
